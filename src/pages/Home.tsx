021b0eee261b2a8c08b977aa8b53c3e89d421f51
Jeeman789 <gavin.r.lacy@gmail.com>
2026-09-07 17:33:17 -0400

    Starting website, forgot how to make a circle :(

---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import '../css/Home.css'
+
+function Home(){
+    return(
+        <div className='container-grounds'>
+            <div className='background'>
+                <div className='section-button'>
+
+                </div>
+                <div className='right-side'>
+                    <div className='section-button'>
+
+                    </div>
+                    <div className='section-button'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='foreground'>
+                <div className='circle'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
