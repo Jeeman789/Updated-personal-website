d5f2aa78966265019adf1fc972f280352ddc3d81
Jeeman789 <gavin.r.lacy@gmail.com>
2026-09-08 15:05:33 -0400

    Fixed middle text, trying to fix outside border

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,25 +6,28 @@ function Home(){
         <div className='container-grounds'>
             <div className='background'>
                 <div id='about-button'>
-
+                    <h5>
+                        ABOUT
+                    </h5>
                 </div>
                 <div id='right-side'>
                     <div id='projects-button'>
-
+                        PROJECTS
                     </div>
                     <div id='contact-button'>
-
+                        CONTACT
                     </div>
                 </div>
             </div>
             <div className='foreground'>
                 <div className='circle'>
-                    <div className='title'>
+                    <div id='title'>
                         <h4>Welcome</h4>
                         <p>
                             Hello! This is the website of Gavin Lacy
-
-                            Check it out and feel free to contact me!
+                        </p>
+                        <p id='second-sentence'>
+                            Take a look around and feel free to contact me!
                         </p>
                     </div>
                 </div>
