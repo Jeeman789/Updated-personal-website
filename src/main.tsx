44a5842f7be0d65a2f17e98aea8241ad96056696
Jeeman789 <gavin.r.lacy@gmail.com>
2026-09-09 20:45:23 -0400

    started about page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import Home from './pages/Home.tsx'
+import About from './pages/About.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <Home />
+  <About />
 )
---
+++ src/pages/About.tsx
@@ -0,0 +1,22 @@
+import '../css/About.css'
+
+function About(){
+    return(
+        <div className='about-back'>
+            <div className='top-bar about'>
+                <h5>
+
+                </h5>
+                <h3>
+                    
+                </h3>
+            </div>
+            <div className='empty-space'></div>
+            <div className='content about'>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
