0e4ed4c718fb4107377112dc3e41765015d290c0
Jeeman789 <gavin.r.lacy@gmail.com>
2026-09-12 11:05:33 -0400

    Added contact page

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import './css/index.css'
 import Home from './pages/Home.tsx'
 import About from './pages/About.tsx'
 import Projects from './pages/Projects.tsx'
+import Contact from './pages/Contact.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <>
   <About />
   <Projects />
+  <Contact />
   </>
 )
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,19 @@
 import '../css/Contact.css'
+import '../css/Other_Pages.css'
 
 function Contact(){
-
+    return(
+        <div className='contact-back'>
+            <div className='top-bar contact'>
+                <h5 className='big-text'>CONTACT</h5>
+                <div className='empty-space-topbar'></div>
+                <h3 className='small-text'>Back</h3>
+            </div>
+            <div className='empty-space'></div>
+            <div className='content-o contact'>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
