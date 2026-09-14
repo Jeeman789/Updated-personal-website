40c08ef49fcf5159fb78efaf5daeae067e0f1f5e
Jeeman789 <gavin.r.lacy@gmail.com>
2026-09-14 12:28:55 -0400

    Updated form elements

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import '../css/Contact.css'
 import '../css/Other_Pages.css'
+import '../css/Text.css'
 
 function Contact(){
     return(
@@ -11,6 +12,16 @@ function Contact(){
             </div>
             <div className='empty-space'></div>
             <div className='content-o contact'>
+                <form id='contact-form' action='/send-email' method='POST'>
+                    <h3 className='small-text'>Send an Email!</h3>
+                    <fieldset id='fields'>
+                        <label htmlFor="name">Name</label>
+                        <input type="name" name='name'></input>
+                        <label htmlFor="body">Message</label>
+                        <textarea id='body' name='body' rows={4} cols={50}></textarea>
+                    </fieldset>
+                    <button id='send-button' type='submit'>SEND</button>
+                </form>
             </div>
         </div>
     )
